58dbd3985ce3329e9d11bca8673daf80257be703
Den-85-ukr <madden85@ukr.net>
2021-02-07 22:28:54 +0200

    Update#3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import colors from "./js/colors.js";
+import colors from "../js/colors.js";
 
 const refs = {
   startBtn: document.querySelector('button[data-action="start"]'),
@@ -18,14 +18,14 @@ function changeColor() {
   refs.stopBtn.addEventListener("click", stopColor);
   interval = setInterval(randomColor, 1000);
   refs.startBtn.disabled = true;
-}
+};
 
 function randomColor() {
   const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
   refs.body.style.backgroundColor = `${color}`;
-}
+};
 
 function stopColor() {
   clearInterval(interval);
   refs.startBtn.disabled = false;
-}
+};
